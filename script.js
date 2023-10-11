8c4df9c299ae35dd05036ddb9849a0a29efbae06
EmmaG44 <emma.gunasekera@gmail.com>
2023-10-11 17:21:39 +0100

    Add playRound function that takes playerSelection and computerSelection and returns string of game result

--- script.js
+++ script.js
@@ -3,4 +3,49 @@ function getComputerChoice() {
     return arr[(Math.floor(Math.random() * arr.length))];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let playerWins = true;
+    const capPlayerSelection = (playerSelection.charAt(0)).toUpperCase() + (playerSelection.substring(1)).toLowerCase();
+    
+    if (capPlayerSelection === computerSelection) {
+        return `It is a draw. Both chose ${capPlayerSelection}`;
+    }
+
+    else {
+    
+        switch(capPlayerSelection) {
+            case "Rock":
+                if (computerSelection === "Paper") {
+                    playerWins = false;
+                }
+                break
+            
+            case "Paper":
+                if (computerSelection === "Scissors") {
+                    playerWins = false;
+                }
+                break
+               
+            case "Scissors":
+                if (computerSelection === "Rock") {
+                    playerWins = false;
+                }
+                break
+                
+            default:
+                return "Not applicable";
+        }
+    }
+
+    if (playerWins) {
+        return `You Win! ${capPlayerSelection} beats ${computerSelection}`;
+    }
+    else {
+        return `You Lose! ${computerSelection} beats ${capPlayerSelection}`
+    }
+
+}
+
+const playerSelection = "pAPER";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
